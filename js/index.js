7f99d86c1e202b004aabd8527d78637098a21bfc
fishka9044 <79632566+fishka9044@users.noreply.github.com>
2021-12-09 18:23:17 +0300

    second-commit

--- js/index.js
+++ js/index.js
@@ -2,39 +2,83 @@
   $('.slider').slick();
  }
 */
+const feedback = document.getElementById('form-select');
+const feedbackSelect = feedback.querySelector('.special-input__select');
+const feedbackInput = feedback.querySelector('.special-input__input');
+
+feedbackSelect.addEventListener('change', function (evt) {
+  feedbackInput.value = `+ ${evt.target.value}`;
+});
+
+const useItForm = document.getElementById('down-form-lesect');
+const useItFormSelect = useItForm.querySelector('.special-input__select');
+const useItFormInput = useItForm.querySelector('.special-input__input');
+
+useItFormSelect.addEventListener('change', function (evt) {
+  useItFormInput.value = `+ ${evt.target.value}`;
+});
 
 $(document).ready(function () {
   $(".slider").slick({
-      arrows:true,
-      dots:true,
-      autoplay:true,
-      infinity: true,
-      autoplaySpeed:500,
-      slidesToShow: 1,
-      centerMode: false,
-      draggable: true,
-      variableWidth: true,
-      responsive: [
-        {
-          breakpoint: 1250,
-          settings: {
-            slidesToShow: 3,
-            arrows:true,
-            centerMode: false,
-            variableWidth: true,
-          }
-        },
-        {
-          breakpoint: 701,
-          settings: {
-            slidesToShow: 1,
-            arrows:true,
-            centerMode: false,
-            variableWidth: true,
-          }
-        }
-      ]
+    arrows:true,
+    dots:true,
+    autoplay:true,
+    infinity: true,
+    autoplaySpeed:1000,
+    slidesToShow: 1,
+    centerMode: false,
+    draggable: true,
+    variableWidth: true,
   });
 });
 
+$(document).ready(function () {
+  $(".slider-mobile").slick({
+    arrows:false,
+    dots:true,
+    autoplay:true,
+    infinity: true,
+    autoplaySpeed:1000,
+    slidesToShow: 1,
+    centerMode: false,
+    draggable: true,
+    variableWidth: true,
+  });
+});
+
+$(document).ready(function(){
+  $(".burger").click(function (e) { 
+    e.preventDefault();
+    $(".menu-burger__list,.burger,.menu-burger").toggleClass("active")
+  });
+})
+
+
+function changeTitleforTablet(father, aim, newWord, oldWord) {
+  let section = document.querySelector(father);
+  let title = section.querySelector(aim);
+  let colorSpan = document.getElementById('orange');
+  if (document.documentElement.clientWidth <= 900) {
+    title.innerHTML = newWord;
+    colorSpan.style.color = 'orange';
+  } 
+  else {
+    title.textContent = oldWord;
+  }
+}
+function changeTitleformobile(father, aim, newWord, oldWord) {
+  let section = document.querySelector(father);
+  let title = section.querySelector(aim);
+  let colorSpan = document.getElementById('orange');
+  if (document.documentElement.clientWidth <= 400) {
+    title.innerHTML = newWord;
+    colorSpan.style.color = 'orange';
+  } 
+  else {
+    title.textContent = oldWord;
+  }
+}
+
+changeTitleforTablet('.comparing','.sub-title-less', 'Отличия функционала версий ПРОФ и КОРП', 'Сравнение функционала версий ПРОФ и КОРП');
+changeTitleformobile('.form','.sub-title-less', `Цена на 1С: Управление сервисным центром в облаке <span><br> от 760 ₽ в месяц</span>`, 'Цена на 1С: ЗУП 8 онлайн от 760 ₽ в месяц');
 

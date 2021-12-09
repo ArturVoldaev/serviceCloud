7a91222e9759ca0f888b5627bdd4500da5b2b298
fishka9044 <79632566+fishka9044@users.noreply.github.com>
2021-12-09 19:42:43 +0300

    product version

--- js/index.js
+++ js/index.js
@@ -1,22 +1,41 @@
-/*if (window.innerWidth <= 1024) {
-  $('.slider').slick();
- }
-*/
 const feedback = document.getElementById('form-select');
 const feedbackSelect = feedback.querySelector('.special-input__select');
 const feedbackInput = feedback.querySelector('.special-input__input');
-
-feedbackSelect.addEventListener('change', function (evt) {
-  feedbackInput.value = `+ ${evt.target.value}`;
-});
 
 const useItForm = document.getElementById('down-form-lesect');
 const useItFormSelect = useItForm.querySelector('.special-input__select');
 const useItFormInput = useItForm.querySelector('.special-input__input');
 
+function changeTitleforTablet(father, aim, newWord) {
+  let section = document.querySelector(father);
+  let title = section.querySelector(aim);
+  let colorSpan = document.getElementById('orange');
+  if (document.documentElement.clientWidth <= 900) {
+    title.innerHTML = newWord;
+    colorSpan.style.color = 'orange';
+  } 
+}
+function changeTitleformobile(father, aim, newWord) {
+  let section = document.querySelector(father);
+  let title = section.querySelector(aim);
+  let colorSpan = document.getElementById('orange');
+  if (document.documentElement.clientWidth <= 400) {
+    title.innerHTML = newWord;
+    colorSpan.style.color = 'orange';
+  } 
+}
+
+changeTitleforTablet('.comparing','.sub-title-less', 'Отличия функционала версий ПРОФ и КОРП');
+changeTitleformobile('.form','.sub-title-less', `Цена на 1С: Управление сервисным центром в облаке <span><br> от 760 ₽ в месяц</span>`);
+
 useItFormSelect.addEventListener('change', function (evt) {
   useItFormInput.value = `+ ${evt.target.value}`;
 });
+
+feedbackSelect.addEventListener('change', function (evt) {
+  feedbackInput.value = `+ ${evt.target.value}`;
+});
+
 
 $(document).ready(function () {
   $(".slider").slick({
@@ -54,31 +73,7 @@ $(document).ready(function(){
 })
 
 
-function changeTitleforTablet(father, aim, newWord, oldWord) {
-  let section = document.querySelector(father);
-  let title = section.querySelector(aim);
-  let colorSpan = document.getElementById('orange');
-  if (document.documentElement.clientWidth <= 900) {
-    title.innerHTML = newWord;
-    colorSpan.style.color = 'orange';
-  } 
-  else {
-    title.textContent = oldWord;
-  }
-}
-function changeTitleformobile(father, aim, newWord, oldWord) {
-  let section = document.querySelector(father);
-  let title = section.querySelector(aim);
-  let colorSpan = document.getElementById('orange');
-  if (document.documentElement.clientWidth <= 400) {
-    title.innerHTML = newWord;
-    colorSpan.style.color = 'orange';
-  } 
-  else {
-    title.textContent = oldWord;
-  }
-}
 
-changeTitleforTablet('.comparing','.sub-title-less', 'Отличия функционала версий ПРОФ и КОРП', 'Сравнение функционала версий ПРОФ и КОРП');
-changeTitleformobile('.form','.sub-title-less', `Цена на 1С: Управление сервисным центром в облаке <span><br> от 760 ₽ в месяц</span>`, 'Цена на 1С: ЗУП 8 онлайн от 760 ₽ в месяц');
+
+
 
